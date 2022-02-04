4d5a6a3220835b0885f365b4e0f4353e18f18810
Eden Baui <eedenbaui@gmail.com>
2022-02-04 20:46:53 +0800

    logo add and bg

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,41 +1,104 @@
 <template>
-  <div class="container-fluid p-4">
-    <img src="" alt="hooter-logo" />
-    <h1 class="greet my-5">
-      <span>Happening now</span>
-    </h1>
-    <h3 class="text-start fw-bold mb-3">Join Hooter today.</h3>
+  <div class="container-fluid p-3">
+    <div class="container p-4">
+      <div class="d-flex">
+        <Logo class="logo-sm" />
+      </div>
+      <h1 class="greet my-5">
+        <span>Happening now</span>
+      </h1>
+      <h3 class="text-start fw-bold mb-3">Join Hooter today.</h3>
 
-    <div class="container d-grid gap-2 p-0">
-      <button type="button" class="btn btn-outline-secondary rounded-pill">
-        Sign in as {Google Auth}
-      </button>
-      <div class="d-flex justify-content-center align-items-center">
-        <span class="horizontal"></span>
-        <span class="mx-2">or</span>
-        <span class="horizontal"></span>
-      </div>
-      <button type="button" class="btn btn-primary rounded-pill">
-        Sign up with email
-      </button>
-      <div class="tos mb-3">
-        <span
-          >By signing up, you agree to the Terms of Service and Privacy Policy,
-          including Cookie Use.</span
+      <div class="container d-grid gap-2 p-0">
+        <button
+          type="button"
+          class="btn btn-outline-secondary rounded-pill btn-max-width"
         >
-      </div>
-      <div class="mt-4 d-grid">
-        <h5 class="fs-6 fw-bold text-start">Already have an account?</h5>
-        <button type="button" class="btn btn-outline-primary rounded-pill">
-          Sign in
+          Sign in as {Google Auth}
         </button>
+        <div
+          class="d-flex justify-content-center align-items-center btn-max-width"
+        >
+          <span class="horizontal"></span>
+          <span class="mx-2">or</span>
+          <span class="horizontal"></span>
+        </div>
+        <button
+          type="button"
+          class="btn btn-secondary rounded-pill btn-max-width"
+        >
+          Sign up with email
+        </button>
+        <div class="tos mb-3 btn-max-width">
+          <span
+            >By signing up, you agree to the Terms of Service and Privacy
+            Policy, including Cookie Use.</span
+          >
+        </div>
+        <div class="mt-4 d-grid mb-2 mt-4">
+          <h5 class="fs-6 fw-bold text-start mb-3">Already have an account?</h5>
+          <button
+            type="button"
+            class="btn btn-outline-secondary rounded-pill btn-max-width mb-2"
+          >
+            Sign in
+          </button>
+        </div>
       </div>
     </div>
   </div>
+  <div class="bg-container">
+    <div class="parallax">
+      <!-- <img src="../assets/boot.png" alt="twitter-bg-edited" class="darken-bg" /> -->
+    </div>
+
+    <Logo class="logo-lg" />
+  </div>
+
+  <footer class="p-3 text-center">
+    <a
+      href="/"
+      class="footer-links link-secondary pe-2 m-1"
+      v-for="(link, i) in footerLinks"
+      :key="link + '-' + i"
+      ><span class="link-text">{{ link }}</span></a
+    >
+    <a href="#" class="footer-links link-secondary pe-2 m-1"
+      >©2022 Hooter, Inc.</a
+    >
+  </footer>
 </template>
 
 <script>
-export default {};
+import Logo from "../components/Logo.vue";
+export default {
+  components: {
+    Logo,
+  },
+  data() {
+    return {
+      footerLinks: [
+        "About",
+        "Help Center",
+        "Terms of Service",
+        "Privacy Policy",
+        "Cookie Policy",
+        "Accessibility",
+        "Ads info",
+        "Blog",
+        "Status",
+        "Careers",
+        "Brand Resources",
+        "Advertising",
+        "Marketing",
+        "Twitter for Business",
+        "Developers",
+        "Directory",
+        "Settings",
+      ],
+    };
+  },
+};
 </script>
 
 <style>
