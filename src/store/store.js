1d90a6fd3f6985c29ed134d51732f2d08e5d4087
Eden Baui <eedenbaui@gmail.com>
2022-02-10 18:37:09 +0800

    add date data in store

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,26 @@ import { createStore } from "vuex";
 export const store = createStore({
   state: {
     isSignUpVisible: false,
+    day: null,
+    month: null,
+    year: null,
+    monthShortName: "",
   },
   mutations: {
     changeSignUpState(state) {
       state.isSignUpVisible = true;
+    },
+    updateDay(state, payload) {
+      state.day = payload;
+    },
+    updateMonth(state, payload) {
+      state.month = payload;
+    },
+    updateYear(state, payload) {
+      state.year = payload;
+    },
+    setMonthShortName(state, payload) {
+      state.monthShortName = payload;
     },
   },
 });
