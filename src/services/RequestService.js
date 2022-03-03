a14ad30606e923579ffa098d771c3f4c67179619
Eden Baui <eedenbaui@gmail.com>
2022-03-03 23:07:13 +0800

    create user get request

--- src/services/RequestService.js
+++ src/services/RequestService.js
@@ -2,16 +2,25 @@ import axios from "axios";
 
 const dburl = "http://localhost:3000/api/hooter/users_db";
 
+//all users
 export async function getHomeData(page) {
   const res = await axios.get(`${dburl}?page=${page}`);
   const data = await res.data;
   return data;
 }
 
+//hoot status
 export async function updateHootStats(uid, hootId, actionName) {
   const res = await axios.patch(
     `${dburl}/${uid}/hoot/${hootId}?action=${actionName}`
   );
   const data = await res.data;
   return data;
+}
+
+//user
+export async function getUser(username) {
+  const res = await axios.get(`${dburl}/user/${username}`);
+  const user = await res.data;
+  return user;
 }
