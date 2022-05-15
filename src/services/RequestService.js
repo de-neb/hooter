07b42c2b132941c567d828878bf9421787a8ba4f
Eden Baui <eedenbaui@gmail.com>
2022-05-15 21:25:54 +0800

    restructure to create instances for api

--- src/services/RequestService.js
+++ src/services/RequestService.js
@@ -1,58 +1,70 @@
 import axios from "axios";
 
-const dburl = "/api/hooter/users_db";
-const trendurl = "/api/hooter/trends";
-const auth = "/api/auth";
+const db = axios.create({
+  baseURL: "/api/hooter/users_db",
+});
+
+const trend = axios.create({
+  baseURL: "/api/hooter/trends",
+});
+
+const auth = axios.create({
+  baseURL: "/api/auth",
+});
+
+// const dburl = "/api/hooter/users_db";
+// const trendurl = "/api/hooter/trends";
+// const auth = "/api/auth";
 
 //all users
 export async function getHomeData(page) {
-  const res = await axios.get(`${dburl}?page=${page}`);
+  const res = await db.get(`?page=${page}`);
   const data = await res.data;
   return data;
 }
 
 //hoot status
-export async function updateHootStats(uid, hootId, actionName) {
-  const res = await axios.patch(
-    `${dburl}/${uid}/hoot/${hootId}?action=${actionName}`
-  );
+export async function updateHootStats(uid, hootId, actionName, otherData) {
+  const res = await db.patch(`/${uid}/hoot/${hootId}?action=${actionName}`, {
+    otherData,
+  });
   const data = await res.data;
   return data;
 }
 
 //user
 export async function getUser(username) {
-  const res = await axios.get(`${dburl}/user/${username}`);
+  const res = await db.get(`/user/${username}`);
   const user = await res.data;
   return user;
 }
 
 export async function createUser(user) {
-  const req = await axios.post(`${auth}/signup`, user);
+  const req = await auth.post(`/signup`, user);
   const res = await req.data;
   return res;
 }
 
 export async function loginUser(userObj) {
-  const req = await axios.post(`${auth}/login`, userObj);
+  const req = await auth.post(`/login`, userObj);
   const res = await req.data;
   return res;
 }
 
 export async function logoutUser(userObj) {
-  const res = await axios.post(`${auth}/logout`, userObj);
+  const res = await auth.post(`/logout`, userObj);
   return res;
 }
 
 export async function userExists(user) {
-  const req = await axios.post(`${auth}/check-user`, user);
+  const req = await auth.post(`/check-user`, user);
   const res = await req.data;
   return res;
 }
 
 //trends
 export async function getTrends() {
-  const res = await axios.get(trendurl);
+  const res = await trend.get("");
   const trends = await res.data;
   return trends;
 }
